47af5229cc443abb51c69fa32f3524285549f093
Daniel_Bohdan <bogdandaniil1309@gmail.com>
2020-09-25 14:58:26 +0300

    create Fields for database

--- src/main/java/db/Fields.java
+++ src/main/java/db/Fields.java
@@ -4,10 +4,26 @@ public final class Fields {
 
     public final static String ID = "id";
 
-    public final static String PRICE = "name";
+    public final static String NAME = "name";
+    public final static String PRICE = "price";
     public final static String COUNT_PEOPLE = "count_people";
     public final static String MARK_HOTEL = "mark_hotel";
     public final static String START_DATE = "start_date";
-    public final static String DAYS = "start_date";
-    public final static String DESCRIPTION = "start_date";
+    public final static String DAYS = "days";
+    public final static String DESCRIPTION = "description";
+    public final static String TYPE_TOUR_ID = "type_tour_id";
+
+    public final static String USERNAME = "username";
+    public final static String PASSWORD = "password";
+    public final static String EMAIL = "email";
+    public final static String STATUS = "status";
+    public final static String ROLE_ID = "role_id";
+
+    public final static String DISCOUNT = "discount";
+    public final static String DISCOUNT_ID = "discount_id";
+    public final static String TOUR_ID = "tour_id";
+    public final static String USER_ID = "user_id";
+
+
+
 }
