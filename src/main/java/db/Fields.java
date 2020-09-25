4e41af1cae2c10983f8099e2668f9e68e29a0c47
Daniel_Bohdan <bogdandaniil1309@gmail.com>
2020-09-25 14:46:22 +0300

    add db-schema

---
+++ src/main/java/db/Fields.java
@@ -0,0 +1,13 @@
+package db;
+
+public final class Fields {
+
+    public final static String ID = "id";
+
+    public final static String PRICE = "name";
+    public final static String COUNT_PEOPLE = "count_people";
+    public final static String MARK_HOTEL = "mark_hotel";
+    public final static String START_DATE = "start_date";
+    public final static String DAYS = "start_date";
+    public final static String DESCRIPTION = "start_date";
+}
